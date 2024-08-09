b08dfdba9bfeba9d67e43caa60c7e048aca0aa60
David Szkiba <david.szkiba@wunderbyte.at>
2024-08-09 09:42:15 +0200

    Bump release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2024062800;
-$plugin->release = '1.0.1';
+$plugin->release = '1.0.2';
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->requires = 2022041900;
 $plugin->component = 'adaptivequizcatmodel_catquiz';
